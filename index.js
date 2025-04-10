45ab1d203939edbeb8618505cab91d129512bc76
Lucas Pereira <lucasdasilvapereira021@gmail.com>
2025-04-10 18:31:15 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,24 @@
-document.getElementById("texto").innerHTML = "Meu primeiro texto <b>JS</b>!";
-alert(10 + 5)
-console.log("Oi , Isso é um console.log")
+// TIPOS DE SAÍDA
+
+document.getElementById("texto").innerHTML = 
+"Meu primeiro texto <b>JS</b>"; // JS IGNORA QUEBRA DE LINHAS
+//alert(10 + 5);
+console.log("Olá, isso é um console.log");
+
+
+// DECLARAÇÕES
+
+let a,b,c;
+a = 5;
+b = 6;
+c = 7;
+
+let pessoa = "Dimitri" ;
+let pesso = "Dimitri"; // JS IGNORA ESPAÇOS EM BRANCOS
+
+function minhafuncao() {
+    document.write('Oi <u>escrito com document.write</u>')
+    document.write('Oi <u>escrito com document.write</u>')
+    document.write('Oi <u>escrito com document.write</u>')
+    document.write('Oi <u>escrito com document.write</u>')
+}
