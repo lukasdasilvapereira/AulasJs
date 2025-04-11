60f0373d8cdf218045867538e8703743c671d945
Lucas Pereira <lucasdasilvapereira021@gmail.com>
2025-04-11 17:32:03 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // TIPOS DE SAÍDA
 
-document.getElementById("texto").innerHTML = 
+//document.getElementById("texto").innerHTML = 
 "Meu primeiro texto <b>JS</b>"; // JS IGNORA QUEBRA DE LINHAS
 //alert(10 + 5);
 console.log("Olá, isso é um console.log");
@@ -8,10 +8,10 @@ console.log("Olá, isso é um console.log");
 
 // DECLARAÇÕES
 
-let a,b,c;
-a = 5;
-b = 6;
-c = 7;
+//let a,b,c;
+//a = 5;
+//b = 6;
+//c = 7;
 
 let pessoa = "Dimitri" ;
 let pesso = "Dimitri"; // JS IGNORA ESPAÇOS EM BRANCOS
@@ -22,3 +22,26 @@ function minhafuncao() {
     document.write('Oi <u>escrito com document.write</u>')
     document.write('Oi <u>escrito com document.write</u>')
 }
+
+// SINTAXES
+
+console.log(5 + 5); // JS ENTENDE COMO SOMAS DE NÚMEROS
+
+console.log("5 + 5"); // JS ENTENDE COMO TEXTO
+
+let a = 5;
+let b = 6;
+let c = a - b;
+
+console.log(c);
+
+// JS É SENSÍVEL A LETRA MAÍSCULA
+
+//var primeiro-nome ISSO SERIA ERRADO PQ O - É USADO PARA SUBTRAÇÃO
+var primeiroNome; // ASSIM SERIA O CERTO
+
+/* 
+
+ISSO É UM COMENTÁRIO
+
+*/
