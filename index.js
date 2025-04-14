67a7d3e16645137391c998ae70cd1a9127637a8e
Lucas Pereira <lucasdasilvapereira021@gmail.com>
2025-04-14 17:59:35 -0300

    Update index.js

--- index.js
+++ index.js
@@ -45,3 +45,35 @@ var primeiroNome; // ASSIM SERIA O CERTO
 ISSO É UM COMENTÁRIO
 
 */
+
+//VARIÁVEIS
+
+// NO JS POSSUIMOS VAR LET E CONST OS MAIS USADO SÃO LET E CONST 
+
+var d,e,f;
+
+d = 5;
+e = 6
+f = d + e;
+
+var nome , sobrenome, nomeCompleto, idade;
+
+nome = "Lucas";
+sobrenome = "Pereira";
+idade = 18;
+nomeCompleto = nome + " " + sobrenome;
+
+pessoa = nome + " " + idade; 
+
+//document.getElementById("texto").innerHTML = nomeCompleto
+
+// A DIFERENÇA ENTRE A VAR PRA OUTRAS , É QUE O VAR PODE SER REEDECLARADO
+
+let x = 10;
+{
+    let x = 2;
+}
+
+console.log("x")
+
+//VAI SER MOSTRADO O NÚMERO 10 PQ ELE ESTÁ NO ESCOPO GERAL, NO VAR NÃO POSSUI DIFERENÇA, IRIA MOSTRAR OQ ESTÁ NO ESCOPO, NO CONST SERIA O MESMO QUE O LET
