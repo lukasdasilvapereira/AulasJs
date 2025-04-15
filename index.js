21a91f28e1e90261f6c66e588fe395c8e6b3ae47
Lucas Pereira <lucasdasilvapereira021@gmail.com>
2025-04-15 13:03:31 -0300

    Update index.js

--- index.js
+++ index.js
@@ -77,3 +77,88 @@ let x = 10;
 console.log("x")
 
 //VAI SER MOSTRADO O NÚMERO 10 PQ ELE ESTÁ NO ESCOPO GERAL, NO VAR NÃO POSSUI DIFERENÇA, IRIA MOSTRAR OQ ESTÁ NO ESCOPO, NO CONST SERIA O MESMO QUE O LET
+
+//OPERADORES
+
+// + - * / = ++ -- += -= && || ETC..
+
+/* SÃO SEPARADOS EM 6 CATEGORIAS
+
+1) ARITMÉTICOS
+2) ATRIBUIÇÃO
+3) SEQUÊNCIA
+4) COMPARAÇÃO
+5) CONDICIONAL
+6) LÓGICOS
+
+*/
+
+var valor1, valor2, valor3;
+valor1 = 5;
+valor2 = 6;
+
+valor3 = valor1 + valor2; // ARITMÉTICO E ATRIBUIÇÃO "=" "+"
+
+console.log(valor3)
+
+// INCREMENTO/DECREMENTO "++(INCREMENTO)" "--(DECREMENTO)"
+
+valor3 = ++valor1;
+
+console.log(valor3);
+console.log(`O valor1 ficou: ${valor1}`);
+
+// ATRIBUIÇÃO
+
+valor1 += valor2 // valor1 = valor1 + valor2
+valor1 -= valor2 // valor1 = valor1 - valor2
+
+// FUNCIONA PRA TODOS OS OPERADORES ARITMÉTICOS
+
+// COMPARAÇÃO : TRUE OR FALSE
+
+var valor1, valor2, valor3;
+valor1 = 10;
+valor2 = 10;
+
+valor3 = (valor1 == valor2); // "==" OPERADOR DE COMPARAÇÃO 
+console.log(valor3);
+
+valor3 = (valor1 === valor2); // "===" OPERADOR DE COMPARAÇÃO PRA SABER SE SÃO IGUAIS EM EXATAMENTE TUDO
+console.log(valor3);
+
+valor3 = (valor1 != valor2); // "!=" OPERADOR DE COMPARAÇÃO "DIFERENTE"
+console.log(valor3);
+
+valor3 = (valor1 !== valor2); // "!==" OPERADOR DE COMPARAÇÃO "DIFERENTE TOTALMENTE"
+console.log(valor3);
+
+valor3 = (valor1 > valor2); // ">" OPERADOR DE COMPARAÇÃO "MAIOR/MENOR/MAIOR-IGUAL/MENOR-IGUAL"
+console.log(valor3);
+
+// < > <= >=
+
+// TERNÁRIO "?"
+var idade, eleitor;
+
+idade = 18;
+
+eleitor = (idade < 18) ? "Não Eleitor":"Sim, Eleitor"; //CONDIÇÃO + ? + SE FOR VERDADEIRO + SE FOR FALSO
+
+console.log(`A resposta é: ${eleitor}. A idade dele é de ${idade} anos`)
+
+// LÓGICOS "||" "&&" "!"
+
+var idade, eleitor, resultado;
+
+idade = 65;
+
+eleitor = (idade < 18) ? "Não Eleitor":"Sim, Eleitor"; //CONDIÇÃO + ? + SE FOR VERDADEIRO + SE FOR FALSO
+
+resultado = (idade > 60 && idade < 70); // TRUE OR FALSE "E"
+resultado = (idade > 60 || idade < 70); // TRUE OR FALSE "OU"
+resultado = !(idade === 60); // TRUE OR FALSE "NÃO"
+
+console.log(resultado);
+
+
