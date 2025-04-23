d9e2fb593ca5e5dda938b6b77e07f300f0a6931e
Lucas Pereira <lucasdasilvapereira021@gmail.com>
2025-04-23 11:47:44 -0300

    .

--- secondpart/script.js
+++ secondpart/script.js
@@ -160,4 +160,12 @@ function verificarCor() {
     }
 }
 
+// LAÇO DE REPETIÇÃO FOR
+
+var ano = new Date().getFullYear();
+
+for (let i = ano; i >= 1900; i--) {
+    document.getElementById("ano").innerHTML += `<option value="${i}">${i}</option>`
+}
+
 
