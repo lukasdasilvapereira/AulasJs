b75397eecacffae4ce7a64c31e53e3450f369d40
Lucas Pereira <lucasdasilvapereira021@gmail.com>
2025-04-24 12:50:17 -0300

    Update scripts.js

--- thirdpart/scripts.js
+++ thirdpart/scripts.js
@@ -24,3 +24,26 @@
  function pareContagem() {
     clearInterval(temp)
  }
+
+ // CLASSES
+
+ class Carro{
+   constructor(valor1, valor2, valor3) {
+      this.marca = valor1;
+      this.modelo = valor2;
+      this.ano = valor3;
+   }
+   buzina() {
+      return this.modelo + ": Biiiiiiii"
+   }
+ }
+
+const uno = new Carro("Fiat", "Uno", 2001)
+const gol = new Carro("Volkswagen", "Gol", 2013);
+
+gol.ano = 2014
+
+console.log(uno)
+console.log(gol)
+console.log(gol.buzina())
+console.log(uno.buzina())
