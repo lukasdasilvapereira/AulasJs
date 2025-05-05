82ee0b6cc8fb55b799e65734147e5c673f1e07ab
Lucas Pereira <lucasdasilvapereira021@gmail.com>
2025-05-05 11:27:22 -0300

    .

--- todolist_page/scripts.js
+++ todolist_page/scripts.js
@@ -1,4 +1,5 @@
 let input = document.getElementById('ipt');
+let contador = 0
 let btnAd = document.getElementById('botaoAdd');
 let principal = document.getElementById('main')
 
@@ -6,15 +7,18 @@ function addTarefa() {
     let valor = input.value;
 
     if((valor !== "") && (valor !== null) && (valor !== undefined)) {
-        let novoItem = ` <div class="item">
-            <div class="item-icone">
-                <i class="fa-regular fa-circle"></i>
+
+        ++contador;
+
+        let novoItem = ` <div  id="${contador}"class="item">
+            <div onclick="marcar(${contador})" class="item-icone">
+                <i id="icone_${contador}" class="fa-regular fa-circle"></i>
             </div>
-            <div class="item-nome">
+            <div  onclick="marcar(${contador})" class="item-nome">
                ${valor}
             </div>
             <div class="item-botao">
-                <button class="delete">Deletar <i class="fa-solid fa-trash"></i></button>
+                <button onclick="deletar(${contador})" class="delete">Deletar <i class="fa-solid fa-trash"></i></button>
             </div>
         </div>`
 
@@ -27,9 +31,37 @@ function addTarefa() {
     }
 }
 
+function marcar(id) {
+    var item = document.getElementById(id);
+    var classe = item.getAttribute("class");
+
+    if(classe == "item") {
+        item.classList.add("clicado")
+
+        var icone = document.getElementById("icone_"+ id);
+        icone.classList.add('fa-circle-check');
+        icone.classList.remove('fa-circle');
+
+        item.parentNode.appendChild(item);
+    }else {
+        item.classList.remove('clicado')
+
+        var icone = document.getElementById("icone_"+ id);
+        icone.classList.remove('fa-circle-check');
+        icone.classList.add('fa-circle');
+    }
+
+}
+
+function deletar(id) {
+    var tarefa = document.getElementById(id);
+    tarefa.remove();
+}
+
 // QUANDO EU CLICAR NO ENTER = 13 N PRECISO CLICAR NO ADICIONAR
 input.addEventListener("keyup", function(event) {
-    if(event, keycode === 13) {
+    if (event.key === "Enter")
+        {
         event.preventDefault();
         btnAd.click();
     }
